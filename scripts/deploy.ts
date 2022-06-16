123489a3c8a580e226316b49e1aed597565d163b
Jose Pedro <josechifflet98@gmail.com>
2022-06-15 21:15:19 -0300

    starter

---
+++ scripts/deploy.ts
@@ -0,0 +1,91 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+import { Contract, ContractFactory } from "ethers";
+
+async function deployDiamond(): Promise<string> {
+  const accounts = await ethers.getSigners();
+  const contractOwner = accounts[0];
+
+  // deploy DiamondCutFacet
+  const DiamondCutFacet = await ethers.getContractFactory("DiamondCutFacet");
+  const diamondCutFacet = await DiamondCutFacet.deploy();
+  await diamondCutFacet.deployed();
+  console.log("DiamondCutFacet deployed:", diamondCutFacet.address);
+
+  // deploy Diamond
+  const Diamond = await ethers.getContractFactory("Diamond");
+  const diamond = await Diamond.deploy(
+    contractOwner.address,
+    diamondCutFacet.address
+  );
+  await diamond.deployed();
+  console.log("Diamond deployed:", diamond.address);
+
+  // deploy DiamondInit
+  // DiamondInit provides a function that is called when the diamond is upgraded to initialize state variables
+  // Read about how the diamondCut function works here: https://eips.ethereum.org/EIPS/eip-2535#addingreplacingremoving-functions
+  const DiamondInit = await ethers.getContractFactory("DiamondInit");
+  const diamondInit = await DiamondInit.deploy();
+  await diamondInit.deployed();
+  console.log("DiamondInit deployed:", diamondInit.address);
+
+  // deploy facets
+  console.log("");
+  console.log("Deploying facets");
+  const FacetNames = ["DiamondLoupeFacet", "OwnershipFacet"];
+  const cut = [];
+  for (const FacetName of FacetNames) {
+    const Facet = await ethers.getContractFactory(FacetName);
+    const facet = await Facet.deploy();
+    await facet.deployed();
+    console.log(`${FacetName} deployed: ${facet.address}`);
+    cut.push({
+      facetAddress: facet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: getSelectors(facet),
+    });
+  }
+
+  // upgrade diamond with facets
+  console.log("");
+  console.log("Diamond Cut:", cut);
+  const diamondCut = await ethers.getContractAt("IDiamondCut", diamond.address);
+  let tx;
+  let receipt;
+  // call to init function
+  let functionCall = diamondInit.interface.encodeFunctionData("init");
+  tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall);
+  console.log("Diamond cut tx: ", tx.hash);
+  receipt = await tx.wait();
+  if (!receipt.status) {
+    throw Error(`Diamond upgrade failed: ${tx.hash}`);
+  }
+  console.log("Completed diamond cut");
+  return diamond.address;
+}
+
+async function main(): Promise<void> {
+  // Hardhat always runs the compile task when running scripts through it.
+  // If this runs in a standalone fashion you may want to call compile manually
+  // to make sure everything is compiled
+  // await run("compile");
+  // We get the contract to deploy
+  const TestTokenFactory: ContractFactory = await ethers.getContractFactory(
+    "TestToken"
+  );
+  const testToken: Contract = await TestTokenFactory.deploy();
+  await testToken.deployed();
+  console.log("TestToken deployed to: ", testToken.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
